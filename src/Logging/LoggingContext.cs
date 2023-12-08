061076e3535bbaa92cc9d6dc8f97163bcdc7e381
cjpomer <jordan.pomeroy@gmail.com>
2023-12-08 15:20:08 -0600

    Add LogLevel to LoggingContext

--- src/Logging/LoggingContext.cs
+++ src/Logging/LoggingContext.cs
@@ -7,6 +7,8 @@ namespace Logging
         private readonly ILogger<T> _logger;
         private readonly LogEntryBuilder _logEntry;
 
+        public LogLevel LogLevel { get; set; } = LogLevel.Information;
+
         public LoggingContext(ILogger<T> logger, LogEntryBuilder logEntry)
         {
             _logger = logger;
@@ -15,7 +17,7 @@ namespace Logging
 
         public void Dispose()
         {
-            _logger.LogInformation(_logEntry.ToString());
+            _logger.Log(LogLevel, _logEntry.ToString());
         }
     }
 }
